b60f9d2fcdac53cf6af1f3f5dafbcab9ad93b20d
Jessie Wang <wangj.5415@gmail.com>
2019-07-10 21:33:46 +0000

    added functionality which checks if a user is logged in, and displays data only specific to that user

--- express_server.js
+++ express_server.js
@@ -14,8 +14,8 @@ app.set("view engine", "ejs");
 
 //all long urls and their generated short URLs
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "4LKOsL" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "4LKOsL" }
 };
 
 //all users (from registration page)
@@ -34,31 +34,34 @@ const users = {
 
 // Routing functions go here
 
-//temporary hompage
-app.get("/", function(req, res) {
-  res.send("Hello! This is the homepage!");
-});
-
-//json object containing our urlDatabase
-app.get("/urls.json", function(req, res) {
-  res.json(urlDatabase);
-})
-
-//random hello function, may delete before submitting project
-app.get("/hello", function(req, res) {
-  res.send("<html><body><b>Hello</b> this is in HTML</body></html>");
-})
-
 //urls page containing the long URLs users have input 
 app.get("/urls", function(req, res) {
   let cookie = req.cookies;
-  let templateVars = {urls: urlDatabase, user: users[cookie.user_id]};
+  // console.log(isUsersLink(urlDatabase, cookie.user_id));
+  let templateVars = {urls: isUsersLink(urlDatabase, cookie.user_id), user: users[cookie.user_id]};
   res.render("urls_index", templateVars);
+})
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  let short = req.params.shortURL;
+  let cookie = req.cookies;
+  let userLinks = isUsersLink(urlDatabase, cookie.user_id);
+  if (userLinks[short]) {
+    delete urlDatabase[short];
+    res.redirect("/urls");
+  } else {
+    res.send("You are not authorized to delete this link.");
+  }
 })
 
 app.get("/urls/new", function(req, res) {
   let cookie = req.cookies;
-  res.render("urls_new", {user: users[cookie.user_id]});
+  //check if user is logged in
+  if (cookie.user_id) {
+    res.render("urls_new", {user: users[cookie.user_id]});
+  } else {
+    res.redirect("/login");
+  }
 })
 
 app.get("/login", (req, res) => {
@@ -75,32 +78,48 @@ app.get("/register", (req, res) => {
 app.get("/urls/:shortURL", function(req, res) {
   let cookie = req.cookies;
   let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[req.params.shortURL];
+  let longURL = urlDatabase[req.params.shortURL].longURL;
   res.render("urls_show", {longURL: longURL, shortURL: shortURL, user: users[cookie.user_id]});
 })
 
+//create short url after inputting long url
 app.post("/urls", (req, res) => {
+  let cookie = req.cookies;
   // console.log(req.body);  // Log the POST request body to the console
   // res.send("Ok");         // Respond with 'Ok' (we will replace this)
   const genshortURL = generateRandomString();
-  urlDatabase[genshortURL] = req.body.longURL;
+  urlDatabase[genshortURL] = {
+    longURL: req.body.longURL, 
+    userID: cookie.user_id
+  };
+  console.log(urlDatabase);
   res.redirect(`/urls/${genshortURL}`);
 });
+
+app.post("/urls/:shortURL/edit", (req, res) => {
+  let cookie = req.cookies;
+  let short = req.params.shortURL;
+  let usersObj = isUsersLink(urlDatabase, cookie.user_id);
+  //check if shortURL exists for current user:
+  if (usersObj[short]) {
+    urlDatabase[short] = {
+      longURL: req.body.longURL, 
+      userID: cookie.user_id
+    };
+    res.redirect("/urls");
+  } else {
+    res.status(403).send("Error 403 - You do not have access to edit this link.");
+  }
+})
 
 app.post("/urls/:shortURL", (req, res) => {
   const short = req.params.shortURL;
   res.redirect(`/urls/${short}`);
 });
 
-app.post("/urls/:shortURL/delete", (req, res) => {
-  let short = req.params.shortURL;
-  delete urlDatabase[short];
-  res.redirect("/urls");
-})
-
 app.post("/login", function(req, res) {
   // if email and password are valid, set the cookie's user_id 
-  console.log(req.cookies);
+  // console.log(req.cookies);
   let userID = checkPassword(req.body.loginemail, req.body.loginpassword);
   if (userID) {
     res.cookie(`user_id`, userID);
@@ -111,7 +130,7 @@ app.post("/login", function(req, res) {
 })
 
 app.post("/logout", function(req, res) {
-  console.log(req.cookies);
+  // console.log(req.cookies);
   res.clearCookie("user_id");
   res.redirect("/urls");
 })
@@ -125,8 +144,6 @@ app.post("/register", function(req, res) {
     res.status(400).send(`Error 400 - That email is already in use!`);
   } else {
     let userID = generateRandomString();
-    res.cookie(`email`, req.body.email);
-    res.cookie(`password`, req.body.password);
     users[userID] = {
       id: userID, 
       email: req.body.email, 
@@ -136,16 +153,10 @@ app.post("/register", function(req, res) {
   res.redirect("/urls");
 })
 
-app.post("/urls/:shortURL/edit", (req, res) => {
-  let short = req.params.shortURL;
-  urlDatabase[short] = req.body.longURL;
-  res.redirect("/urls");
-})
-
 //redirect to longURL link:
 app.get("/u/:shortURL", function(req, res) {
   let shortURL = req.params.shortURL;
-  res.redirect(urlDatabase[shortURL]);
+  res.redirect(urlDatabase[shortURL].longURL);
 })
 
 // Start the server
@@ -181,4 +192,15 @@ function checkPassword(email, password) {
       return users[user].id;
     }
   }
+}
+
+//check if link belongs to user
+function isUsersLink(object, id) {
+  let returned = {};
+  for (let obj in object) {
+    if (object[obj].userID == id) {
+      returned[obj] = object[obj];
+    }
+  }
+  return returned;
 }
